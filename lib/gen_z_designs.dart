ff10962eb3a8a56f75003fb5ac19b6fd5ed395d4
Kaushal Zod <kaushal@azafashions.com>
2022-09-05 15:56:20 +0530

    Countdown package exported in main file

--- lib/gen_z_designs.dart
+++ lib/gen_z_designs.dart
@@ -8,3 +8,4 @@ export 'package:gen_z_designs/position/gen_z_position.dart';
 export 'package:gen_z_designs/shape/gen_z_button_shape.dart';
 export 'package:gen_z_designs/types/gen_z_button_types.dart';
 export 'package:gen_z_designs/constant/gen_z_constant.dart';
+export 'package:gen_z_designs/component/gen_z_countdown.dart';
